9d19203eb033cbdb86e4308e11d92f2f8a334015
Squagilli <cotrupeconnor@gmail.com>
2021-06-29 17:43:41 +0200

    trying to get game function to work

--- rps.js
+++ rps.js
@@ -1,9 +1,3 @@
-let draws = 0;
-let wins = 0;
-let losses = 0;
-
-const playerSelection;
-const computerSelection;
 
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']; // list computer choices
@@ -14,23 +8,65 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     // Ask player for selection
-    playerSelection = prompt('Rock, Paper, Scissors?');
     // Make computer select
-    computerSelection = computerPlay();
     // compare comp & player selections
+    if (playerSelection === computerSelection) {
+        draws++
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        losses++
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        losses++
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        losses++
+    }
+    else {
+        wins++
+    }
 
     // store result in draw, win, lose
-    console.log(playRound(playerSelection, computerSelection));
+    if (wins > 0) {
+        console.log(`Computer chose: ${computerSelection}
+        \nYou chose: ${playerSelection}
+        \n${playerSelection} beats ${computerSelection}
+        \nYou won!`);
+    }
+    else if (losses > 0) {
+        console.log(`Computer chose ${computerSelection}
+        \nYou chose: ${playerSelection}
+        \n${computerSelection} beats ${playerSelection}
+        \nYou lost!`);
+    }
+    else if (draws > 0) {
+        console.log(`It's a draw! You both chose ${playerSelection}`);
+    }
+    return(playerSelection, computerSelection)
 }
+const playerSelection = prompt('Rock, Paper, Scissors?');
+const computerSelection = computerPlay();
+
+let draws = 0;
+let wins = 0;
+let losses = 0;
 
 function game() {
     // play round five times
+    for (let i = 0; i < 5; i++) {
+        playRound()
+    }
     // store # of wins, losses, draws
     // report winner
+    if (wins > losses) {
+        console.log(`You won ${wins} out of 5 rounds! There were ${draws} draws.`);
+    }
+    else if (losses > wins) {
+        console.log(`You lost ${losses} out of 5 rounds! There were ${draws} draws.`);
+    }
 }
 
-
-
+console.log(playRound(playerSelection, computerSelection));
 
 
 
