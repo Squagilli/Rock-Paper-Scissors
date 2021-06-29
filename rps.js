46ace51596020f101674f5cf50241cdfd61dd870
Squagilli <cotrupeconnor@gmail.com>
2021-06-29 19:58:45 +0200

    fixed functions to work as intended.

--- rps.js
+++ rps.js
@@ -1,4 +1,13 @@
 
+
+
+let draws = 0;
+let wins = 0;
+let losses = 0;
+let win;
+let loss;
+let draw;
+
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors']; // list computer choices
     let computerChoice = choices[Math.floor(Math.random() * choices.length)]; // comp chooses random
@@ -6,68 +15,96 @@ function computerPlay() {
      
 }
 
+function playerPlay() {
+    let playerChoice = prompt("Rock, Paper, Scissors?");
+    return playerChoice;
+}
+
 function playRound(playerSelection, computerSelection) {
     // Ask player for selection
     // Make computer select
     // compare comp & player selections
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
+        console.log(`It's a draw. You both chose ${playerSelection}`);
         draws++
+        draw = true
     }
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log(`Computer chose: ${computerSelection}
+        \nYou chose: ${playerSelection}
+        \n${computerSelection} beats ${playerSelection}
+        \nYou lost.`);
         losses++
+        loss = true;
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log(`Computer chose: ${computerSelection}
+        \nYou chose: ${playerSelection}
+        \n${computerSelection} beats ${playerSelection}
+        \nYou lost.`);
         losses++
+        loss = true
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log(`Computer chose: ${computerSelection}
+        \nYou chose: ${playerSelection}
+        \n${computerSelection} beats ${playerSelection}
+        \nYou lost.`);
         losses++
+        loss = true;
     }
-    else {
-        wins++
-    }
-
-    // store result in draw, win, lose
-    if (wins > 0) {
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
         console.log(`Computer chose: ${computerSelection}
         \nYou chose: ${playerSelection}
         \n${playerSelection} beats ${computerSelection}
         \nYou won!`);
+        wins++
+        win = true;
     }
-    else if (losses > 0) {
-        console.log(`Computer chose ${computerSelection}
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        win = true;
+        console.log(`Computer chose: ${computerSelection}
         \nYou chose: ${playerSelection}
-        \n${computerSelection} beats ${playerSelection}
-        \nYou lost!`);
+        \n${playerSelection} beats ${computerSelection}
+        \nYou won!`);
+        wins++
     }
-    else if (draws > 0) {
-        console.log(`It's a draw! You both chose ${playerSelection}`);
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        win = true;
+        console.log(`Computer chose: ${computerSelection}
+        \nYou chose: ${playerSelection}
+        \n${playerSelection} beats ${computerSelection}
+        \nYou won!`);
+        wins++
     }
     return(playerSelection, computerSelection)
 }
-const playerSelection = prompt('Rock, Paper, Scissors?');
-const computerSelection = computerPlay();
+const playerSelection = '';
+const computerSelection = '';
 
-let draws = 0;
-let wins = 0;
-let losses = 0;
+
 
 function game() {
     // play round five times
     for (let i = 0; i < 5; i++) {
-        playRound()
+        playRound(playerSelection, computerSelection);
     }
     // store # of wins, losses, draws
     // report winner
     if (wins > losses) {
-        console.log(`You won ${wins} out of 5 rounds! There were ${draws} draws.`);
+        console.log(`\nYou win! You won ${wins} out of 5 rounds! ${draws} draw(s).`);
     }
     else if (losses > wins) {
-        console.log(`You lost ${losses} out of 5 rounds! There were ${draws} draws.`);
+        console.log(`\nComputer wins. You lost ${losses} out of 5 rounds. ${draws} draw(s).`);
+    }
+    else {
+        console.log(`Draw. You won ${wins} rounds and lost ${losses} out of 5 rounds. ${draws} draw(s).`);
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-
+game();
 
 
 
