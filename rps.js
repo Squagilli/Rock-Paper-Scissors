af29118d50c7a3ff1ff7302507879f14d01a56aa
Squagilli <cotrupeconnor@gmail.com>
2021-07-01 17:17:23 +0200

    trying to make image replace itself on click

--- rps.js
+++ rps.js
@@ -11,16 +11,103 @@ const roundResult = document.getElementById('roundResult');
 const playerScore = document.getElementById('scoreplayer');
 const compScore = document.getElementById('scoreComp');
 
-const rock1 = img.src='images/rock1.png';
+const playerSelection = '';
+const computerSelection = '';
 
-rockSelect.addEventListener('click', e => {
-    playerWeapon.append(rock1); 
-})
+let playerChoice = '';
 
+const img = '';
+const src = ''; 
+
+function clearImg() {
+    playerWeapon.removeChild();
+}
+// player selection button
+function playerPlay(){
+    rockSelect.addEventListener('click', function(){
+        computerPlay();
+        const img = document.createElement('img');
+        img.className = 'rock';
+        img.id = 'image';
+        img.src = 'images/rock1.png';
+        const src = document.getElementById('playerWep');
+        src.appendChild(img);
+
+        
+        
+    })
+    paperSelect.addEventListener('click', function(){
+        computerPlay();
+        const img = document.createElement('img');
+        img.className = 'paper';
+        img.id = 'image';
+        img.src = 'images/paper1.png';
+        const src = document.getElementById('playerWep');
+        src.appendChild(img);
+        
+        
+    })
+    scissorSelect.addEventListener('click', function(){
+        computerPlay();
+        const img = document.createElement('img');
+        img.className = 'scissors';
+        img.id = 'image';
+        img.src = 'images/scissors1.png';
+        const src = document.getElementById('playerWep');
+        src.appendChild(img);
+        
+    })
+    
+    /*function chooseRock() {
+        computerPlay()
+        playerChoice = 'rock';
+    }
+    function choosePaper() {
+        computerPlay()
+        playerChoice = 'paper';
+    }
+    function chooseScissors() {
+        computerPlay()
+        playerChoice = 'scissors';
+    }*/
+    
+    return playerChoice;
+}
+
+// computer select function
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    if (computerChoice === 'rock') {
+        const img = document.createElement('img');
+        img.className = 'rock';
+        img.src = 'images/rock1.png';
+        const src = document.getElementById('compWep');
+        src.appendChild(img);
+    }
+    else if (computerChoice === 'paper') {
+        const img = document.createElement('img');
+        img.className = 'paper';
+        img.src = 'images/paper1.png';
+        const src = document.getElementById('compWep');
+        src.appendChild(img);
+    }
+    else if (computerChoice === 'scissors') {
+        const img = document.createElement('img');
+        img.className = 'scissors';
+        img.src = 'images/scissors1.png';
+        const src = document.getElementById('compWep');
+        src.appendChild(img);
+    }
+}
+
+
+playerPlay();
 
 // create new game function
+    // on click new game 
 
-// create play game function
+    // create play game function
     // player play
     // computer play 
 
