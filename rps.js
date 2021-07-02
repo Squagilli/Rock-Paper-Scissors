430604b29b5988ef4b28f48fea20596fd920af30
Squagilli <cotrupeconnor@gmail.com>
2021-07-02 21:44:17 +0200

    Thanks to stack overflow the functions modify the HTML as intended.

--- rps.js
+++ rps.js
@@ -3,6 +3,7 @@ const newButton = document.getElementById('newGameButton');
 const rockSelect = document.getElementById('rockPicture');
 const paperSelect = document.getElementById('paperPicture');
 const scissorSelect = document.getElementById('scissorPicture');
+const reloadButton = document.querySelector('.newGame');
 
 // image or text fields to be manipulated
 const playerWeapon = document.getElementById('playerWep');
@@ -10,16 +11,20 @@ const compWeapon = document.getElementById('compWep');
 const pWepLabel = document.querySelector('.playerWeaponLabel');
 const compWepLabel = document.querySelector('.compWeaponLabel');
 const roundResult = document.getElementById('roundResult');
-const playerScore = document.getElementById('scoreplayer');
+const playerScore = document.getElementById('scorePlayer');
 const compScore = document.getElementById('scoreComp');
 const img = '';
 const src = ''; 
 
 // player onclick function
+let playerChoice = 'default';
+let draws = 0;
+let wins = 0;
+let losses = 0;
 
-let playerChoice = '';
 
 function playerPlay() {
+
     
     rockSelect.addEventListener('click', function(){
         chooseRock();
@@ -33,6 +38,29 @@ function playerPlay() {
         img.src = 'images/rock1.png';
         const src = document.getElementById('playerWep');
         src.appendChild(img);
+
+
+        playerChoice = 'rock';
+        console.log(playerChoice);
+
+        if (computerChoice === 'rock') {
+            roundResult.innerHTML = `Tie. Both chose Rock.`
+            draws++
+            console.log(draws);
+        }
+        else if (computerChoice === 'scissors') {
+            roundResult.innerHTML = `Player wins! Rock beats Scissors.`
+            wins++
+            console.log(wins);
+            playerScore.innerHTML = `Player Score: ${wins}`
+        }
+        else if (computerChoice === 'paper') {
+            roundResult.innerHTML = `Comp wins. Paper beats Rock.`
+            losses++
+            console.log(losses);
+            compScore.innerHTML = `Computer Score: ${losses}`
+        }
+
 
      
     })
@@ -49,6 +77,27 @@ function playerPlay() {
         const src = document.getElementById('playerWep');
         src.appendChild(img);
 
+
+        playerChoice = 'paper';
+        console.log(playerChoice);
+
+        if (computerChoice === 'rock') {
+            roundResult.innerHTML = `Player wins! Paper beats Rock.`
+            wins++
+            console.log(wins);
+            playerScore.innerHTML = `Player Score: ${wins}`
+        }
+        else if (computerChoice === 'scissors') {
+            roundResult.innerHTML = `Comp wins. Scissors beats Paper.`
+            losses++
+            console.log(losses);
+            compScore.innerHTML = `Computer Score: ${losses}`
+        }
+        else if (computerChoice === 'paper') {
+            roundResult.innerHTML = `Tie. Both chose Paper.`
+            draws++
+            console.log(draws);
+        }
         
       
     })
@@ -64,6 +113,30 @@ function playerPlay() {
         img.src = 'images/scissors1.png';
         const src = document.getElementById('playerWep');
         src.appendChild(img);
+
+
+        playerChoice = 'scissors';
+        console.log(playerChoice);
+
+        if (computerChoice === 'rock') {
+            roundResult.innerHTML = `Comp wins. Rock beats Scissors.`
+            losses++
+            console.log(losses);
+            compScore.innerHTML = `Computer Score: ${losses}`
+        }
+        else if (computerChoice === 'scissors') {
+            roundResult.innerHTML = `Tie. Both chose Scissors`
+            draws++
+            console.log(draws);
+        }
+        else if (computerChoice === 'paper') {
+            roundResult.innerHTML = `Player wins! Scissors beats Paper.`
+            wins++
+            console.log(wins);
+            playerScore.innerHTML = `Player Score: ${wins}`
+        }
+
+
         
     })
     
@@ -73,11 +146,9 @@ function playerPlay() {
     }
     function choosePaper() {
         pWepLabel.innerHTML = 'Paper';
-        
     }
     function chooseScissors() {
         pWepLabel.innerHTML = 'Scissors';
-        
     }
 
     return playerChoice;
@@ -105,9 +176,8 @@ function computerPlay() {
         const src = document.getElementById('compWep');
         src.appendChild(img);
 
-        cRock = true;
-
         compWepLabel.innerHTML = 'Rock';
+        console.log(computerChoice);
     }
     else if (computerChoice === 'paper') {
         if (document.getElementById('imageC')) {
@@ -120,9 +190,8 @@ function computerPlay() {
         const src = document.getElementById('compWep');
         src.appendChild(img);
 
-        cPaper = true;
-
         compWepLabel.innerHTML = 'Paper';
+        console.log(computerChoice);
     }
     else if (computerChoice === 'scissors') {
         if (document.getElementById('imageC')) {
@@ -135,9 +204,8 @@ function computerPlay() {
         const src = document.getElementById('compWep');
         src.appendChild(img);
 
-        cScissors = true;
-
         compWepLabel.innerHTML = 'Scissors';
+        console.log(computerChoice);
     }
 
     return computerChoice;
